e4e76756a8c37e86a56e557dcac361e4d0790c92
Sonal Rajendra Meshram <sonalrmeshram@gmail.com>
2025-08-07 15:30:55 +0530

    statelock

--- day-4-statelock/backend.tf
+++ day-4-statelock/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "sonalrmeshram"
     key    = "day-4/terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true  #s3 support lastest version >1.10.0
+    #use_lockfile = true  #s3 support lastest version >1.10.0
+    dynamodb_table = "test" #apply for any version
+    encrypt = true
   }
 } 
--- day-4-statelock/main.tf
+++ day-4-statelock/main.tf
@@ -2,13 +2,6 @@ resource "aws_instance" "dev" {
   ami           = "ami-084a7d336e816906b"
   instance_type = "t2.micro"
   tags = {
-    Name ="dev" 
+    Name ="test" 
   }
-}
-resource "aws_vpc" "name" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "devops"
   }
-  
-}
