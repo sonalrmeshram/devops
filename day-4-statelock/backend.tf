df374bbccc511a6870c95ac9f0a2759aa4139e77
Sonal Rajendra Meshram <sonalrmeshram@gmail.com>
2025-08-08 07:17:45 +0530

    -state-lock

--- day-4-statelock/backend.tf
+++ day-4-statelock/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "day-4/terraform.tfstate"
     region = "us-east-1"
     #use_lockfile = true  #s3 support lastest version >1.10.0
-    dynamodb_table = "test" #apply for any version
-    encrypt = true
+   # dynamodb_table = "dev" #apply for any version
+    #encrypt = true
   }
 } 
